b17c598895232d636800e98eee55e9be598915dc
Sidharth <downloadall616@gmail.com>
2022-01-01 19:02:17 +0530

    added links to login and signup pages

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import "./login.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./login.css";
 
 function Login() {
-    return (
-        <div className="loginWrapper">
-            <div className="loginMain">
-                <h1>WOMZONE</h1>
-                <input placeholder="Username" type="text"/>
-                <input placeholder="Password" type="password"/>
-                <button className="buttonLoginAndRegister">Login</button>
-                <div className="signUp">Sign Up</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="loginWrapper">
+      <div className="loginMain">
+        <h1>WOMZONE</h1>
+        <input placeholder="Username" type="text" />
+        <input placeholder="Password" type="password" />
+        <button className="buttonLoginAndRegister">Login</button>
+        <Link to="/register" style={{textDecoration:"none", color:"black",width:"100%"}}>
+          <div className="signUp">Sign Up</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import "../Login/login.css"
+import { Link } from "react-router-dom";
+import React from "react";
+import "../Login/login.css";
 
 function Register() {
-    return (
-        <div className="loginWrapper">
-            <div className="signUpMain">
-                <h1>WOMZONE</h1>
-                <input placeholder="Full name" type="text"/>
-                <input placeholder="Email" type="email"/>
-                <input placeholder="Password" type="password"/>
-                <button className="buttonLoginAndRegister">Sign Up</button>
-                <div className="signUp">Login</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="loginWrapper">
+      <div className="signUpMain">
+        <h1>WOMZONE</h1>
+        <input placeholder="Full name" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button className="buttonLoginAndRegister">Sign Up</button>
+        <Link to="/login" style={{textDecoration:"none", color:"black",width:"100%"}}>
+          <div className="signUp">Login</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
